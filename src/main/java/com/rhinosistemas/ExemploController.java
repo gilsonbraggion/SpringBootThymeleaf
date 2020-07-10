694c0c731931e99f43aab1428f3c4e19ba6b6af7
Gilson Braggion <gilsonbraggion@MacBook-Pro-de-Gilson.local>
2020-07-10 19:09:41 -0300

    Inclusão do projeto no Git

---
+++ src/main/java/com/rhinosistemas/ExemploController.java
@@ -0,0 +1,15 @@
+package com.rhinosistemas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/exemplo")
+public class ExemploController {
+	
+	@RequestMapping(value = "/primeiro")
+	public String primeiro() {
+		return "exemplo/primeiro";
+	}
+	
+}
